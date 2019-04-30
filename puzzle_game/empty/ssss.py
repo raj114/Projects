f733fc8a59f0651e692be89190c0f43bea42d93c
raj114 <rajendragarade@gmail.com>
2019-04-30 12:59:33 +0530

    Commit

--- puzzle_game/empty/ssss.py
+++
@@ -1,5 +0,0 @@
-from Tkinter.t import *
-root = t.Tk()
-root.configure(background='bisque')
-t.insert("raj")
-root.mainloop()
--- puzzle_game/empty/xyz.py
+++
@@ -1,8 +0,0 @@
-import sys
-import tkinter
-from tkinter import *
-
-Tk = tkinter.Tk
-root = Tk()
-lb=Label(root,text="enter no:",height=12)
-root.mainloop()
